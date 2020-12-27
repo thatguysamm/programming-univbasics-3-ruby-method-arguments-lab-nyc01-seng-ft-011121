e0ccadc09caed51cdecd46e26b2747235d64b9e6
friendly-format-0074 <samuelagasi@gmail.com>
2020-12-27 15:19:44 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,10 +2,17 @@ def introduction(name)
   puts "Hi, my name is #{name}"
 end
 
+def introduction_with_language(name, language)
+  puts "Hi,my name is #{name} and i am learning to program in #{language}"
+end
+
+def introduction_with_language_optional(name, language= Ruby)
+  puts "Hi,my name is #{name} and i am learning to program in #{language}"
+end
+
 introduction
-
-
-
+introduction_with_language
+introduction_with_language_optional
 
 
 
